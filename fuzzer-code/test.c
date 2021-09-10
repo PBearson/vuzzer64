b432a2ca2e857ec97ef5853e72452184015fab44
PBearson <peabryan95@gmail.com>
2021-09-10 12:43:10 -0400

    Added string comparison into test.c

--- fuzzer-code/test.c
+++ fuzzer-code/test.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 int main(int argc, char* argv[])
 {
@@ -28,6 +29,17 @@ int main(int argc, char* argv[])
         return a / b;
     }
 
+    char* output_sub = malloc(4);
+
+    strncpy(output_sub, output, 4);
+    printf("First 4 bytes: %s\n", output_sub);
+    int cmp = strcmp(output_sub, "BAD!");
+    if(cmp == 0)
+    {
+        printf("Oh no, BAD! cannot start the string!\n");
+        abort();
+    }
+
     if(output[28] == 'A')
     {
         if(output[29] == 'B')
@@ -36,7 +48,7 @@ int main(int argc, char* argv[])
             {
                 if(output[31] == 'D')
                 {
-                    printf("ABORT\n");
+                    printf("Oh no, ABCD cannot end the string!\n");
                     abort();
                 }
             }
