8a98f791572b6cb656ede53c01df62c43c978583
PBearson <peabryan95@gmail.com>
2021-09-14 16:51:18 -0400

    Any pickle filenames now supported

--- fuzzer-code/unpickle.py
+++ fuzzer-code/unpickle.py
@@ -1,10 +1,33 @@
 import pickle
+import sys
 
-f = open("test.pkl", "r")
+arg1 = sys.argv[1]
+arg2 = sys.argv[2]
+
+pkl = ""
+names = ""
+
+if "pkl" == arg1[-3:]:
+    pkl = arg1
+elif "pkl" == arg2[-3:]:
+    pkl = arg2
+else:
+    print "ERROR: pkl file not supplied"
+    exit(-1)
+
+if "names" == arg1[-5:]:
+    names = arg1
+elif "names" == arg2[-5:]:
+    names = arg2
+else:
+    print "ERROR: names file not supplied"
+    exit(-1)
+
+f = open(pkl, "r")
 x = pickle.load(f)
 f.close()
-
-f = open("test.names", "r")
+    
+f = open(names, "r")
 y = pickle.load(f)
 y1 = [i for i in y[0]]
 y2 = [i for i in y[1]]
